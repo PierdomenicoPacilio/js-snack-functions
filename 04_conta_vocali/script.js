7a855e187b7711923dc7a9cc1ec82a88e8ccaadf
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-03-03 16:36:33 +0100

    feat: 04 completed

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,19 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function vowelsIn (string) {
+    let count = 0;
+    for (i = 0; i < string.length; i++){
+        if(string[i].toLowerCase() === 'a' || string[i].toLowerCase() === 'e' || string[i].toLowerCase() === 'i' || string[i].toLowerCase() === 'o' || string[i].toLowerCase() === 'u'){
+            count += 1;
+        };
+    };
+    return count;
+};
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(`le vocali in "${word}" sono ${vowelsIn(word)}`);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
