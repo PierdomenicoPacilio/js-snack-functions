97e72048b2b5b67aec0295a8a713fac250f3d3c4
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-03-03 16:23:32 +0100

    feat: 03 completed

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,10 +4,17 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function filtredArray (userArray, char){
+    const returnAarrey = [];
+    for (i = 0; i < userArray.length; i++){
+        if (userArray[i][0].toLowerCase() === char.toLowerCase()) returnAarrey.push(userArray[i]);    
+    };
+    return returnAarrey;
+};
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(filtredArray(names, 'A'));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
