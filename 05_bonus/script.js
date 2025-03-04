99b3618c874680f0c5e421f848dcf25943427910
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-03-04 11:27:40 +0100

    feat: 05 bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,24 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function userHello(nome) {
+    const ora = new Date().getHours();
+    let hello;
+    if (ora < 13) {
+        hello = "Buongiorno";
+    } else if (ora < 17) {
+        hello = "Buon pomeriggio";
+    } else {
+        hello = "Buonasera";
+    };
+    return `${hello} ${nome}`;
+};
 
+// Esempio di utilizzo
+console.log(`${new Date().getHours()}:${new Date().getMinutes()}`);
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(userHello(name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
